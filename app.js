29d9b10ecf87da10f1fe3bfc4a6af2e3c4850474
subhadra30 <k.subhadra@gmail.com>
2024-07-13 21:34:26 +0100

    first update

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log("Hello, World!");
+const images = ["pics/bay.jpg", "pics/hill.jpg", "pics/tunnel.jpg"];
+
+let currentIndex = 0;
+
+const largeImage = document.getElementById("largeimage");
+
+const firstImage = document.getElementById("first");
+firstImage.addEventListener("click", loadImage1);
+function loadImage1() {
+  currentIndex = 0;
+  largeImage.src = images[currentIndex];
+}
+const secondImage = document.getElementById("second");
+secondImage.addEventListener("click", loadImage2);
+function loadImage2() {
+  currentIndex = 1;
+  largeImage.src = images[currentIndex];
+}
+const thirdImage = document.getElementById("third");
+thirdImage.addEventListener("click", loadImage3);
+function loadImage3() {
+  currentIndex = 2;
+  largeImage.src = images[currentIndex];
+}
+const nextButton = document.getElementById("next-button");
+nextButton.addEventListener("click", loadNextimage);
+function loadNextimage() {
+  if (currentIndex == 2) {
+    currentIndex = -1;
+  }
+  currentIndex++;
+  largeImage.src = images[currentIndex];
+}
+const prevButton = document.getElementById("previous-button");
+prevButton.addEventListener("click", loadPreviousimage);
+function loadPreviousimage() {
+  if (currentIndex <= 0) {
+    currentIndex = 3;
+  }
+  currentIndex--;
+  largeImage.src = images[currentIndex];
+}
